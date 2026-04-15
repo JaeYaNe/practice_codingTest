4c130f3c155e65665331540a7a39ffec2b919e36
JaeYaNe <kmj01103@naver.com>
2026-04-15 17:27:53 +0900

    .

--- pg_138476.py
+++ pg_138476.py
@@ -1,20 +1,19 @@
 from collections import Counter
 
 def solution(k, tangerine):
-
-    a = Counter(tangerine)
-    a = dict(sorted(a.items(), key=lambda x: x[1], reverse=True))
-
-    c = 0
-    for key, value in a.items():
-        
-        if(k - value > 0):
-            k -= value
-            c += 1
-            continue
-        else :
-            c += 1
-            return c
+    # 1. 개수 세기
+    counts = Counter(tangerine)
     
+    # 2. 개수(value)만 뽑아서 내림차순 정렬 (key는 사실 몰라도 되니까요!)
+    sorted_counts = sorted(counts.values(), reverse=True)
+    
+    answer = 0
+    for count in sorted_counts:
+        k -= count  # 일단 뺀다
+        answer += 1 # 종류 하나 추가
+        if k <= 0:  # 다 채웠으면 종료
+            break
+            
+    return answer
 
 print(solution(6,	[1, 3, 2, 5, 4, 5, 2, 3]	))
