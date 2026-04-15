99b0f3ec8b0fae8d0f2073efb7b98fd2697b6c04
JaeYaNe <kmj01103@naver.com>
2026-04-15 17:24:16 +0900

    .

---
+++ pg_138476.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+def solution(k, tangerine):
+
+    a = {}
+    a = Counter(tangerine)
+    a = dict(sorted(a.items(), key=lambda x: x[1], reverse=True))
+
+    c = 0
+    for key, value in a.items():
+        
+        if(k - value > 0):
+            k -= value
+            c += 1
+            continue
+        else :
+            c += 1
+            return c
+    
+
+print(solution(6,	[1, 3, 2, 5, 4, 5, 2, 3]	))
---
+++ pg_42885.py
@@ -0,0 +1,21 @@
+# 최대 2명, limit 존재, 50000번
+
+def solution(people, limit):
+    people.sort() 
+    
+    answer = 0
+    left = 0              
+    right = len(people) - 1  
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1  
+
+        right -= 1
+        answer += 1
+        
+    return answer
+
+
+
+print(solution([70,50,80,50], 100))
